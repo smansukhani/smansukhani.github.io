53b4270b30f4eaf1871c03c05876ca06dbb547b4
smansukhani <s.mansukhani@ardencahillacademy.com>
2022-02-25 18:36:23 +0000

    making sure my website is up to date

--- projects/catstagram/javascript/brains.js
+++ projects/catstagram/javascript/brains.js
@@ -1,1 +1,9 @@
+var counter1 = 0;
 
+function likePhoto() {
+    counter1++;
+    console.log(counter1);
+    document.getElementById("likeCount1").innerHTML = counter1;
+
+}
+onClick = likePhoto();
