16ac8b67b092a7db5d9af31deda28f4581753893
smansukhani <s.mansukhani@ardencahillacademy.com>
2022-03-14 19:04:34 +0000

    completed project

--- projects/catstagram/javascript/brains.js
+++ projects/catstagram/javascript/brains.js
@@ -1,9 +1,21 @@
-var counter1 = 0;
-
-function likePhoto() {
-    counter1++;
-    console.log(counter1);
-    document.getElementById("likeCount1").innerHTML = counter1;
-
+var likeCount = [0,0,0,0,0];
+function likePhoto(num) {
+        likeCount[num]++;
+        console.log(likeCount);
+        document.getElementById("likeCount" + num).innerHTML = 
+    likeCount[num];
 }
-onClick = likePhoto();
+var user = {
+    name: 'Felines ' + "n'" + ' Fame',
+    name_last:'',
+    profile_pic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhI6pA_MIWZ2LwW31LR8M5qRz7pueahd38lQ&usqp=CAU",
+    followers: ['mina','madison','kittens','seas','elijah'],
+    bio: 'See what your famous felines are up to today!',
+    username: 'feline_friends',
+}
+setTimeout(function(){ document.getElementById("name").innerHTML = user.name;}, 3000);
+ setTimeout(function(){ document.getElementById("name_last").innerHTML = user.name_last;}, 3000);
+ setTimeout(function(){ document.getElementById("followers").innerHTML = "Followers: " + user.followers.length;}, 3000);
+ setTimeout(function(){ $("#profile_pic").attr("src", user.profile_pic);}, 3000);
+ setTimeout(function(){ document.getElementById("bio").innerHTML = user.bio;}, 3000);
+ setTimeout(function(){ document.getElementById("username").innerHTML = user.username;}, 3000);
