ab0efbd92cfe7f06d5d5d04a99ea90ba6a444fc6
smansukhani <s.mansukhani@ardencahillacademy.com>
2022-03-17 12:39:05 +0000

    finished project

--- projects/catstagram/javascript/brains.js
+++ projects/catstagram/javascript/brains.js
@@ -2,14 +2,13 @@ var likeCount = [0,0,0,0,0];
 function likePhoto(num) {
         likeCount[num]++;
         console.log(likeCount);
-        document.getElementById("likeCount" + num).innerHTML = 
-    likeCount[num];
+        document.getElementById("likeCount" + num).innerHTML = likeCount[num];
 }
 var user = {
     name: 'Felines ' + "n'" + ' Fame',
     name_last:'',
     profile_pic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhI6pA_MIWZ2LwW31LR8M5qRz7pueahd38lQ&usqp=CAU",
-    followers: ['mina','madison','kittens','seas','elijah'],
+    followers: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,27,56,79,79,79,79,7,9,8,88,8,9,9,9,9,9,9,9,9,9,99,9,99,9,9,9,9,99,9,9,9,9,99,9,9,9,9,99,9],
     bio: 'See what your famous felines are up to today!',
     username: 'feline_friends',
 }
