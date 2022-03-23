c366e4d65dd8771ff1d651d820208eabc06a9271
smansukhani <s.mansukhani@ardencahillacademy.com>
2022-03-23 18:40:46 +0000

    saving work - not finished

--- projects/runtime/js/init.js
+++ projects/runtime/js/init.js
@@ -32,11 +32,12 @@ var init = function (window) {
     ground = opspark.makeGround(app);
 
     // TODO 2 : add background
-
+     var background = opspark.makeBackground(app,ground);
+     view.addChild(background);
     
     var help = draw.textfield('MOVES || up: jump | right: flying jump | down: duck | space: fire | q self destruct!', 
-        '20px Arial',
-        '#ccc', 'left');
+        '20px georgia',
+        'white', 'left');
     help.x = 10;
     help.y = ground.y + ground.getBounds().height + 10;
     view.addChild(help);
@@ -71,7 +72,9 @@ var init = function (window) {
     }
     
     // TODO 1 : add a heads-up display to game
-
+        var hud = opspark.makeHud();
+        view.addChild(hud);
+        window.hud = hud;
 
 
     var game = opspark.createGameManager(app,hud);
--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -26,27 +26,56 @@ var background = function (window) {
         var background;
         
         // ANIMATION VARIABLES HERE:
-        
+        var tree;
+        var buildings = [];
      
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
         function render() {
             background.removeAllChildren();
 
+            
             // TODO: 2 - Part 2
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'#009A17');
             background.addChild(backgroundFill);
-            
+
+            var backgroundFill = draw.rect(canvasWidth, groundY,'#447272');
+            background.addChild(backgroundFill);
+
+
             // TODO: 3 - Add a moon and starfield
-            
+
+            var moon = draw.bitmap('img/moon.png');
+            moon.x = 1200;
+            moon.y = 20;
+            moon.scaleX = 0.3;
+            moon.scaleY = 0.3;
+            background.addChild(moon);  
+
+            var circles = 0;
+            for (circles = 100; circles < 1001; circles++ ) {
+                var circle = draw.circle(2,'white','LightGray',2);
+                circle.x = canvasWidth*Math.random();
+                circle.y = groundY*Math.random()-20;
+                background.addChild(circle);
+            }
             
             // TODO 5: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
-            
+            for (var i = 0; i < 7; ++i) {
+                var buildingHeight = Math.random() * 400;
+                var building =draw.rect(75,buildingHeight,'black','white',5);
+                building.x = 200 * i;
+                building.y = groundY - buildingHeight;
+                background.addChild(building);
+                buildings.push(building);
+            } 
             // TODO 4: Part 1 - Add a tree
-            
+            tree = draw.bitmap('img/tree.png');
+            tree.x = 200;
+            tree.y = 60;
+            background.addChild(tree); 
             
         } // end of render function - DO NOT DELETE
         
@@ -60,10 +89,20 @@ var background = function (window) {
             var groundY = ground.y;
             
             // TODO 4: Part 2 - Move the tree!
-            
+            tree.x = tree.x - 2;
+        
+            if(tree.x < -200) {
+             tree.x = canvas.width;
+            }
+           
             
             // TODO 5: Part 2 - Parallax
-            
+            for(var i = 0; i < buildings.length; i++) {
+                buildings[i].x = buildings[i].x - 1;
+                 if(buildings[i].x < -200) {
+                buildings[i].x = canvasWidth;
+          }
+        }
 
         } // end of update function - DO NOT DELETE
         
